a681b9b6e3b89413e8e5096a3ee1f45890530896
Jee_desctop <Jee_desctop>
2021-05-23 17:46:52 +0900

    all added

---
+++ numpyandpandas.py
@@ -0,0 +1,16 @@
+
+import numpy as np
+
+A=np.array( [[1,2],[3,4]])
+
+print(A)
+
+print(type(A))
+
+print( A.ndim)
+print(A.shape)
+print(A.dtype )
+
+
+print( A.max() , A.mean() , A.min(), A.sum())
+
